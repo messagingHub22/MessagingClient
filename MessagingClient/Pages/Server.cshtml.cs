878eac935e889e39a5055c0602738e6aff33516b
Yajat Kumar <yajatapps3@gmail.com>
2022-09-29 15:45:24 -0700

    Add a new page from where the messages will be sent to server

---
+++ MessagingClient/Pages/Server.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MessagingClient.Pages
+{
+    public class ServerModel : PageModel
+    {
+        private readonly ILogger<ServerModel> _logger;
+
+        public ServerModel(ILogger<ServerModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
