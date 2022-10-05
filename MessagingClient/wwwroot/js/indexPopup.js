bf4aa18e8c7e462470b8e6f4f22ed1312b397231
Yajat Kumar <yajatapps3@gmail.com>
2022-10-05 00:51:48 -0700

    Move the popup related js and css to different files

---
+++ MessagingClient/wwwroot/js/indexPopup.js
@@ -0,0 +1,17 @@
+﻿const popupQuerySelector = "#myPopup";
+const popupEl = document.querySelector(popupQuerySelector);
+const popupBttn = document.querySelector("#button");
+popupBttn.addEventListener("click", () => {
+    setTimeout(() => {
+        if (!popupEl.classList.contains("show")) {
+            popupEl.classList.add("show");
+        }
+    }, 250);
+});
+
+document.addEventListener("click", (e) => {
+    const isClosest = e.target.closest(popupQuerySelector);
+    if (!isClosest && popupEl.classList.contains("show")) {
+        popupEl.classList.remove("show");
+    }
+});
