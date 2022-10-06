3e9594bed20834e6639e1be6fb04a73eae7b7ea5
Yajat Kumar <yajatapps3@gmail.com>
2022-10-06 13:10:15 -0700

    Add controller for sending requests to library and connect server page to it

---
+++ MessagingClient/Controllers/LibraryController.cs
@@ -0,0 +1,17 @@
+﻿using MessagingLibrary;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MessagingClient.Controllers
+{
+    // Controller to send data to the messaging library
+    [Route("libApi")]
+    [ApiController]
+    public class LibraryController : ControllerBase
+    {
+        [HttpPost("libSendMessage")]
+        public void LibSendMessage(String SentTime, String Content, String MessageCategory, String MessageUser)
+        {
+            MessagingAPI.SendMessage(SentTime, Content, MessageCategory, MessageUser);
+        }
+    }
+}
