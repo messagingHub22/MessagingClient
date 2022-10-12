a9b6d3ad7a3ad24ad56f4641c5337f1c412f81a2
Yajat Kumar <yajatapps3@gmail.com>
2022-10-12 13:34:31 -0700

    Move index javascript to new file

---
+++ MessagingClient/wwwroot/js/index.js
@@ -0,0 +1,24 @@
+﻿// Set the initial unread messages count
+let messageCount = @unreadMessagesCount;
+
+document.getElementById("popupBadge").innerHTML = messageCount;
+
+function markMessageRead(element, id) {
+    element.parentElement.style.background = "#e6e6e6";
+    element.style.visibility = 'hidden';
+
+    let url = "libApi/libMarkMessageRead?Id=" + id;
+    fetch(url, { method: 'POST' });
+
+    messageCount--;
+    document.getElementById("popupBadge").innerHTML = messageCount;
+}
+
+function loginUser() {
+    let userName = document.getElementById('userName').value;
+
+    let loginForm = document.getElementById('loginForm');
+    loginForm.innerHTML = '';
+
+    document.getElementById('loginIntro').innerHTML = 'Logged in user: ' + userName;
+}
